99f78a70c779bd22a710abb0f7eef76c4f04a5fc
isthisnagee <isthisnagee@gmail.com>
2018-02-16 21:44:09 -0500

    add hello world

--- source/app.py
+++ source/app.py
@@ -38,6 +38,10 @@ def hello_world():
 
     return jsonify(r)
 
+@app.route('/test')
+def test():
+    return "hello world!"
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
