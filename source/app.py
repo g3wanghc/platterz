48e2ed890e1ebb6b985be97dbcad773cd40fbc83
hanchen.wang <hanchen.wang@gmail.com>
2018-02-16 22:39:25 -0500

    sdsadasd

--- source/app.py
+++ source/app.py
@@ -12,7 +12,36 @@ cache = {}
 
 @app.route('/api', methods=['POST'])
 def api():
-    content = request.get_json()
+    action_1 = [
+        make_action('food', 'apple'),
+        make_action('food', 'oranges'),
+        make_action('food', 'bananas'),
+    ]
+
+    action_2 = [
+        make_action('colour', 'red'),
+        make_action('colour', 'yellow'),
+        make_action('colour', 'blue'),
+    ]
+
+    action_3 = [
+        make_action('spicyness', 'extreme'),
+        make_action('spicyness', 'hot'),
+        make_action('spicyness', 'mild'),
+    ]
+
+    attach_1 = make_attachment('Choose a food: ', 'callback_id_food', action_1)
+    attach_2 = make_attachment('Choose a colour: ', 'callback_id_colour', action_2)
+    attach_3 = make_attachment('Choose a spicyness: ', 'callback_id_spicyness', action_3)
+
+    attachments = [attach_1, attach_2, attach_3] * times_called
+
+    r = make_response('Dietary Prefrences: ', attachments)
+    return jsonify(r)
+
+@app.route('/interaction', methods=['POST'])
+def callback():
+        content = request.get_json()
 
     user = content['user']['id'];
     value = 0
@@ -62,11 +91,6 @@ def api():
 
     r = make_response('Dietary Prefrences: ', attachments)
     return jsonify(r)
-
-@app.route('/callback', methods=['POST'])
-def callback():
-    content = request.get_json()
-    return jsonify(content)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
