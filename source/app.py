df1788604ec4f4338a2f01dfe0a6e2ce448e8926
hanchen.wang <hanchen.wang@gmail.com>
2018-02-16 20:08:31 -0500

    prompt maker

--- source/app.py
+++ source/app.py
@@ -1,6 +1,35 @@
 from flask import Flask
+from helpers import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+
+    action_1 = {
+        make_action('food', 'apple'),
+        make_action('food', 'oranges'),
+        make_action('food', 'bananas'),
+    }
+
+    action_2 = {
+        make_action('colour', 'red'),
+        make_action('colour', 'yellow'),
+        make_action('colour', 'blue'),
+    }
+
+    action_3 = {
+        make_action('spicyness', 'extreme'),
+        make_action('spicyness', 'hot'),
+        make_action('spicyness', 'mild'),
+    }
+
+    actions = [action_1, action_2, action_3]
+
+    attach_1 = make_attachment('Choose an option: ', 'callback_id_001', actions)
+
+    attachments = [attach_1]
+
+    r = make_response('Hello, World!', attachments)
+
+    return json.dumps(r).replace("\'", "\"")
---
+++ source/helpers.py
@@ -0,0 +1,34 @@
+import json
+
+def make_action(name, text):
+	button = {
+		'name': name,
+		'text': text,
+		'type': 'button',
+		'value': ''.join([c for c in text.lower() if c.isalnum()])
+	}
+	return button
+
+def make_attachment(attachement_prompt, callback_id, actions_arr):
+	actions = []
+	for action in actions_arr:
+		actions.append(action)
+
+	attachment = {
+		'text': attachement_prompt,
+		'callback_id': callback_id,
+		'actions': actions
+	}
+	return attachment
+
+def make_response(prompt_question, attachments_arr):
+	attachments = []
+	for attachment in attachments_arr:
+		attachments.append(attachment)
+
+	response = {
+		'text': prompt_question,
+		'attachments': attachments
+	}
+
+	return response
