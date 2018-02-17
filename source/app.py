832f2aa62ef27daf4c482094faf0448b0f9a9ae2
hanchen.wang <hanchen.wang@gmail.com>
2018-02-16 20:14:32 -0500

    correct format for slack prompt generation

--- source/app.py
+++ source/app.py
@@ -6,30 +6,32 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    action_1 = {
+    action_1 = [
         make_action('food', 'apple'),
         make_action('food', 'oranges'),
         make_action('food', 'bananas'),
-    }
+    ]
 
-    action_2 = {
+    action_2 = [
         make_action('colour', 'red'),
         make_action('colour', 'yellow'),
         make_action('colour', 'blue'),
-    }
+    ]
 
-    action_3 = {
+    action_3 = [
         make_action('spicyness', 'extreme'),
         make_action('spicyness', 'hot'),
         make_action('spicyness', 'mild'),
-    }
+    ]
 
-    actions = [action_1, action_2, action_3]
+    attach_1 = make_attachment('Choose a food: ', 'callback_id_food', action_1)
 
-    attach_1 = make_attachment('Choose an option: ', 'callback_id_001', actions)
+    attach_2 = make_attachment('Choose a colour: ', 'callback_id_colour', action_2)
 
-    attachments = [attach_1]
+    attach_3 = make_attachment('Choose a spicyness: ', 'callback_id_spicyness', action_3)
 
-    r = make_response('Hello, World!', attachments)
+    attachments = [attach_1, attach_2, attach_3]
+
+    r = make_response('Dietary preferences: ', attachments)
 
     return json.dumps(r).replace("\'", "\"")
